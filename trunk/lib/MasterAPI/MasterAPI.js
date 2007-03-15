512788312e9788b9a2bdbd870b9a0ecda5d579b1
pdp.gnucitizen <pdp.gnucitizen@ebc9117e-2020-0410-9799-33ba4fc751fc>
2007-03-15 00:47:53 +0000

    Initial Deport!

--- trunk/lib/MasterAPI/MasterAPI.js
+++
@@ -1,5 +0,0 @@
-var MasterAPI = {
-	version: '0.1.9a',
-	author: 'Petko Petkov | pdp (architect)',
-	homepage: 'http://www.gnucitizen.org'};
-
--- trunk/lib/ZombieAPI/ZombieAPI.js
+++
@@ -1,4 +0,0 @@
-var ZombieAPI = {
-	version: '0.2a',
-	author: 'Petko Petkov | pdp (architect)',
-	homepage: 'http://www.gnucitizen.org'};
