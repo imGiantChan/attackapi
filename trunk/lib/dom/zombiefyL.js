ba0e2788e5ded5d2716e2419774ad63ac321a7db
pdp.gnucitizen <pdp.gnucitizen@ebc9117e-2020-0410-9799-33ba4fc751fc>
2007-12-02 12:27:09 +0000

    more cleanup

--- trunk/lib/dom/zombiefyL.js
+++
@@ -1,8 +0,0 @@
-
-AttackAPI.zombiefyL = function (url, interval) {
-	var interval = (interval == 'undefined')?interval:2000;
-
-	window.setInterval(function () {
-		this.requestJSL(url + '?action=pull');
-	}, interval);
-};
