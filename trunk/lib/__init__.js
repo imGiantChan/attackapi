b54e2564c49ecf486f070f97820ad5fc8a861c33
pdp.gnucitizen <pdp.gnucitizen@ebc9117e-2020-0410-9799-33ba4fc751fc>
2011-02-10 16:08:43 +0000

    start clean trunk

--- trunk/lib/__init__.js
+++
@@ -1,6 +0,0 @@
-
-var AttackAPI = {
-	version: '3.0.0a',
-	author: 'Petko D. Petkov | pdp',
-	homepage: 'http://www.gnucitizen.org',
-	projecthome: 'http://www.gnucitizen.org/projects/attackapi'};
--- trunk/lib/dom/requestXSSL.js
+++
@@ -1,9 +0,0 @@
-
-AttackAPI.requestXSSL = function (url) {
-	var ifr = document.createElement('iframe');
-	ifr.style.visibility = 'hidden';
-	ifr.style.width = ifr.style.height = 0;
-	ifr.src = url;
-	
-	document.body.appendChild(ifr);
-};
